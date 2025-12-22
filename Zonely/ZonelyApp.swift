ae675c2f089774e0566c1bb217d7c18bf12b7d0f
shihabshaharia <shihabsdev@gmail.com>
2025-12-23 00:19:12 +0600

    Initial Commit

---
+++ Zonely/ZonelyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZonelyApp.swift
+//  Zonely
+//
+//  Created by Shihab Shaharia on 23/12/25.
+//
+
+import SwiftUI
+
+@main
+struct ZonelyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
